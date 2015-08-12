fa28436925dc27786eb75f09a85b03201d6f5093
Thomas Hartmann <thomi.hartmann@gmail.com>
2015-08-12 16:26:30 +0200

    * step 7 * init step 5

---
+++ step5_simple_ml/README.md
@@ -0,0 +1,12 @@
+Step 5: Precision Definition with Simple Machine Learning
+======================
+ 
+This step of the KMF tutorial will...
+
+
+Simple Machine Learning
+-------------
+
+
+Precision Definition
+------------------
---
+++ step5_simple_ml/smartcity.mm
@@ -0,0 +1,20 @@
+class smartcity.City {
+    att name : String
+    ref* districts: smartcity.District
+}
+
+class smartcity.District {
+    att name: String
+    ref contact: smartcity.Contact
+    ref* sensors: smartcity.Sensor
+}
+
+class smartcity.Contact {
+    att name: String
+    att email: String
+}
+
+class smartcity.Sensor {
+    att name: String
+    att value: Double
+}
