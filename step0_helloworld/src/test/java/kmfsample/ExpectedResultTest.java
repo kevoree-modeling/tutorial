d3bdee019e7da9de06b4b91e744d53fe7f701a87
Francois Fouquet <fouquet.f@gmail.com>
2015-12-10 10:57:35 +0100

    change the excepted result

--- step0_helloworld/src/test/java/kmfsample/ExpectedResultTest.java
+++ step0_helloworld/src/test/java/kmfsample/ExpectedResultTest.java
@@ -44,14 +44,6 @@ public class ExpectedResultTest {
 
         Assert.assertEquals("NewCreatedCity==>{\"universe\":0,\"time\":0,\"uuid\":1,\"data\":{\"name\":\"MySmartCity\"}}\n" +
                 "ModifiedCity==>{\"universe\":0,\"time\":0,\"uuid\":1,\"data\":{\"name\":\"MySmartCity\",\"districts\":[2,3]}}\n" +
-                "FullModel:[\n" +
-                "{\"@class\":\"smartcity.City\",\"@uuid\":1,\"name\":\"MySmartCity\",\"districts\":[2,3]},\n" +
-                "{\"@class\":\"smartcity.District\",\"@uuid\":2,\"name\":\"District_1\",\"op_City_districts\":[1]},\n" +
-                "{\"@class\":\"smartcity.District\",\"@uuid\":3,\"name\":\"District_2\",\"sensors\":[4],\"op_City_districts\":[1]},\n" +
-                "{\"@class\":\"smartcity.Sensor\",\"@uuid\":4,\"name\":\"TempSensor_0\",\"value\":\"0.5\",\"op_District_sensors\":[3]}\n" +
-                "]\n" +
-                "\n" +
-                "ResolvedRoot====> {\"universe\":0,\"time\":0,\"uuid\":1,\"data\":{\"name\":\"MySmartCity\",\"districts\":[2,3]}}\n" +
                 "Navigated districts:\n" +
                 "{\"universe\":0,\"time\":0,\"uuid\":2,\"data\":{\"name\":\"District_1\",\"op_City_districts\":[1]}}\n" +
                 "{\"universe\":0,\"time\":0,\"uuid\":3,\"data\":{\"name\":\"District_2\",\"sensors\":[4],\"op_City_districts\":[1]}}\n" +
@@ -60,8 +52,14 @@ public class ExpectedResultTest {
                 "Districts extracted:2\n" +
                 "{\"universe\":0,\"time\":0,\"uuid\":2,\"data\":{\"name\":\"District_1\",\"op_City_districts\":[1]}}\n" +
                 "{\"universe\":0,\"time\":0,\"uuid\":3,\"data\":{\"name\":\"District_2\",\"sensors\":[4],\"op_City_districts\":[1]}}\n" +
-                "Sensor extracted:1\n" +
-                "{\"universe\":0,\"time\":0,\"uuid\":4,\"data\":{\"name\":\"TempSensor_0\",\"value\":0.5,\"op_District_sensors\":[3]}}\n", buffer.toString());
+                "FullModel:[\n" +
+                "{\"@class\":\"smartcity.City\",\"@uuid\":1,\"name\":\"MySmartCity\",\"districts\":[2,3]},\n" +
+                "{\"@class\":\"smartcity.District\",\"@uuid\":2,\"name\":\"District_1\",\"op_City_districts\":[1]},\n" +
+                "{\"@class\":\"smartcity.District\",\"@uuid\":3,\"name\":\"District_2\",\"sensors\":[4],\"op_City_districts\":[1]},\n" +
+                "{\"@class\":\"smartcity.Sensor\",\"@uuid\":4,\"name\":\"TempSensor_0\",\"value\":\"0.5\",\"op_District_sensors\":[3]}\n" +
+                "]\n" +
+                "\n" +
+                "CityByFindMethod==>{\"universe\":0,\"time\":0,\"uuid\":1,\"data\":{\"name\":\"MySmartCity\",\"districts\":[2,3]}}\n", buffer.toString());
 
         System.out.println(buffer.toString());
     }
